37786bc2eb40ee28d9ecc22e9d2408833bee5d95
mwbullen <mwbullen@mikes-mac-mini.home>
2014-08-26 13:40:37 -0400

    Started adding cannon tracking

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -17,14 +17,26 @@ public class Cannon : MonoBehaviour {
 
 	public GameObject primaryStation;
 
+	public float maxHTurnAngle = 85f;
+
+	//public Vector3 targetRotation;
+
+	private Vector3 resetPosition;
+
+	public float rotateSpeed = 5f;
+
 	// Use this for initialization
 	void Start () {
-
+		resetPosition = cannonBase.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timeSinceLastFire += Time.deltaTime;
+
+		if (currentTarget != null) {
+			RotatetoTarget(currentTarget);
+				}
 	}
 
 	void Fire () {
@@ -61,9 +73,9 @@ public class Cannon : MonoBehaviour {
 
 		float angle = .5f * Mathf.Asin (9.81f * targetDistance / Mathf.Pow(cannonVelocity,2));
 
-		Debug.Log (targetDistance);
-		Debug.Log (angle);
-		//Need to rotate on X axis
+//		Debug.Log (targetDistance);
+//		Debug.Log (angle);
+//		//Need to rotate on X axis
 
 //		if (targetDistance > 500) {
 //
@@ -71,11 +83,25 @@ public class Cannon : MonoBehaviour {
 	}
 
 	void RotatetoTarget(GameObject g) {
-//		Vector3 relativePos = g.transform.position - cannonBase.transform.position;
+		Vector3 adjustedTargetPosition = new Vector3 (g.transform.position.x, cannonBase.transform.position.y, g.transform.position.z);
+
+		//Vector3 relativePos = adjustedTargetPosition - cannonBase.transform.position;
+		Vector3 relativePos = adjustedTargetPosition - resetPosition;
+
 //		Quaternion rotation = Quaternion.LookRotation(relativePos);
 //		cannonBase.transform.rotation = rotation;
 //
-		cannonBase.transform.LookAt (g.transform.position);
+		//cannonBase.transform.LookAt (g.transform.position);
+
+		float angletoTarget = Vector3.Angle (resetPosition, relativePos);
+
+		Debug.Log (angletoTarget + ", " + maxHTurnAngle);
+		if (angletoTarget <= maxHTurnAngle) {
+				cannonBase.transform.rotation = Quaternion.Slerp (cannonBase.transform.rotation, Quaternion.LookRotation (relativePos), rotateSpeed * Time.deltaTime);
+				} else {
+			cannonBase.transform.rotation = Quaternion.Slerp (cannonBase.transform.rotation, Quaternion.LookRotation (resetPosition), rotateSpeed * Time.deltaTime);
+		}
+		//Debug.Log (Vector3.Angle (transform.forward, relativePos));
 	}
 	
 	
